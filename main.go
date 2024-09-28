f8f579426b8e8cebc9028a58ee50b6d32e86080e
Cristi104 <petrecristian2004@gmail.com>
2024-09-28 23:28:54 +0300

    Basic image loading test

--- main.go
+++ main.go
@@ -9,20 +9,29 @@ import (
 
 type Game struct{}
 
+var image *ebiten.Image
+
 func (g *Game) Update() error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, "Hello, World!")
+	screen.DrawImage(image, nil)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	// return 480, 270
+	return 240, 135
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
+	var err error
+	image, _, err = ebitenutil.NewImageFromFile("res/textures/Button.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	ebiten.SetWindowSize(1600, 900)
 	ebiten.SetWindowTitle("Hello, World!")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
