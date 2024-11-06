ba368b42f1a99e4faf4f1a46d54e90abe91ac023
Cristi104 <petrecristian2004@gmail.com>
2024-11-06 18:44:08 +0200

    Camera test

--- main.c
+++ main.c
@@ -17,13 +17,38 @@ int main() {
     GmlibText *text = GmlibTextCreate("Lorem ipsum testus maximus", (Vector2) {10, 10}, NULL, 8, 20);
     GmlibMap map = GmlibMapCreate(11, 11);
 
+    Camera2D camera = {{0, 0}, {0, 0}, 0, 1};
+
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(GRAY);
+
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+            camera.zoom += 0.03;
+        }
+        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+            camera.zoom -= 0.03;
+        }
+        if (IsKeyDown(KEY_W)) {
+            camera.offset.y += 10;
+        }
+        if (IsKeyDown(KEY_A)) {
+            camera.offset.x += 10;
+        }
+        if (IsKeyDown(KEY_S)) {
+            camera.offset.y -= 10;
+        }
+        if (IsKeyDown(KEY_D)) {
+            camera.offset.x -= 10;
+        }
+
+        BeginMode2D(camera);
+
+        GmlibMapDraw(map);
+
+        EndMode2D();
+
         GmlibTextDraw(text);
-
-//        GmlibMapDraw(map);
-
 //        DrawTexturePro(*GmlibGetTexture("Grass.png"), (Rectangle) {0, 0, 256, 256}, (Rectangle) {0, 0, 512, 512}, (Vector2) {0, 0}, 0, WHITE);
         DrawFPS(settings.resolutionWidth - 100, 0);
         EndDrawing();
